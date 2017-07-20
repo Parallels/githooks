a36f9ba2cef3878e0d45834620c6f230d03daa5a
Anna Tikhonova <anna.m.tikhonova@gmail.com>
2017-07-20 19:06:08 +0300

    Do not process commits that exist in repository (rejectmerge hook)

--- hooks.d/rejectmerge.py
+++ hooks.d/rejectmerge.py
@@ -53,7 +53,7 @@ class Hook(object):
 
         permit = True
 
-        log = hookutil.parse_git_log(self.repo_dir, branch, old_sha, new_sha)
+        log = hookutil.parse_git_log(self.repo_dir, branch, old_sha, new_sha, this_branch_only=False)
 
         messages = []
         for commit in log:
